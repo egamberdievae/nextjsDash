e0cb446e693d38bdaa4218947e155446077ebe12
Ezoza <egamberdiyevae2@gmail.com>
2024-01-26 15:27:46 +0500

    nextjs added

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Dashboard = () => {
+  return (
+    <h1>Dashboard</h1>
+  )
+}
+
+Dashboard.propTypes = {}
+
+export default Dashboard
